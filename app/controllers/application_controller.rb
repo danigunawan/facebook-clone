1fddb1ef97723a5fcb34fda0c547402dc047a366
Tripple-A <biodun9@gmail.com>
2019-12-11 12:38:56 +0100

    added the form for user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name , :last_name])
+      end
+
+      def after_sign_in_path_for(resource)
+        #users_show_url
+      end
+    ​
+      def after_sign_out_path_for(resource)
+        # return the path based on resource
+      end
+   
+    
+    
+    
+    
+    
 end
