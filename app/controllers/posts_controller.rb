85a70b270b3bad8b78437d5c007478a373d7c9d7
Izaias Neto <izaiasneto4@gmail.com>
2019-12-11 15:54:50 -0300

    Added the post field to the homepage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
     before_action :authenticate_user!
     def index 
-        @posts = Post.all
+      @posts = Post.all
+      @post = Post.new
     end
 
     def new
