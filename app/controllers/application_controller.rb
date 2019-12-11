8d72821f033a45826299708dcf4198149bbbede3
Tripple-A <biodun9@gmail.com>
2019-12-11 18:00:31 +0100

    generated the Post model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,6 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name , :last_name])
-      end
-
-      def after_sign_in_path_for(resource)
-        #users_show_url
-      end
-    ​
-      def after_sign_out_path_for(resource)
-        # return the path based on resource
-      end
-   
-    
-    
-    
-    
+    end
     
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
+   
+    def home
+    end
     
    def index 
    end
