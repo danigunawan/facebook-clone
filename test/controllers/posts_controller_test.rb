607a08401c42567e4314499c98212d33f3f773cd
Biodun <biodun9@gmail.com>
2019-12-12 20:30:54 +0100

    linted

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -5,7 +5,6 @@ require 'test_helper'
 class PostsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-
   test 'should see only sign in unless signed in' do
     get new_user_session_path
     assert_response :success
