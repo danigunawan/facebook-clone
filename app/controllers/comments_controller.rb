727847cd8e38678f6a26376d9d4c2d78c4f128b5
Biodun <biodun9@gmail.com>
2019-12-16 12:37:15 +0100

    added comment section and implemented relationships

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,15 @@
 class CommentsController < ApplicationController
+
+    def create 
+        @comment = current_user.comments.build(comment_params)
+        @comment.save
+        redirect_to root_path
+    end
+
+    private
+    
+    def comment_params 
+        params.require(:comment).permit(:post_id,:comment)
+    end
     
 end
