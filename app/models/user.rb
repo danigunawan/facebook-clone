9b8e54153545c24c8d0ad0a39a9c8e1b3194744d
Biodun <biodun9@gmail.com>
2019-12-19 17:00:56 +0100

    removed comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@
 class User < ApplicationRecord
   include Gravtastic
   gravtastic
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
   has_many :posts
