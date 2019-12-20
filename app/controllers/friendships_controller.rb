f2ffc0caeac40f32fc62f9f12389e2e68620c800
Biodun <biodun9@gmail.com>
2019-12-20 23:50:04 +0100

    restored to previous state

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,7 +6,9 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    @friendship = current_user.friendships.build(friend_id: params[:friendship][:user_id])
+    count = Friendship.where(user_id: current_user.id, friend_id: params[:friendship][:user_id]).count ||
+            Friendship.where(friend_id: current_user.id, user_id: params[:friendship][:user_id]).count
+    @friendship = current_user.friendships.build(friend_id: params[:friendship][:user_id]) if count.zero?
     if @friendship&.save
       flash[:success] = 'Friend request succesfully sent'
       redirect_to users_path, notice: 'You just sent a request'
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,4 +5,9 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
   validates :user_id, presence: true
   validates :friend_id, presence: true
+  validate :check_request
+
+  def check_request
+    errors.add(:invalid_friend_request, 'Cannot befriend self') if user_id == friend_id
+  end
 end
